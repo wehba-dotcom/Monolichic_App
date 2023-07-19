9d5971f79ec3a467870460818e06bc0ec28df5ea
wehba-dot <cph-wk7@cphbusiness.dk>
2023-07-19 10:06:53 +0200

    refactoring

--- ASP.NETCoreIdentityCustom/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ ASP.NETCoreIdentityCustom/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -82,7 +82,7 @@ namespace ASP.NETCoreIdentityCustom.Areas.Identity.Pages.Account
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Husk mig?")]
             public bool RememberMe { get; set; }
         }
 
